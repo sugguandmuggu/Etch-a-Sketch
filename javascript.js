4018cbc50349fce654b0095250a9258f1634ccb2
Saksham <jaiswalsaksham32@gmail.com>
2022-08-09 15:37:57 +0530

    add container

---
+++ javascript.js
@@ -0,0 +1,8 @@
+//const body = document.querySelector(body);
+
+const container = document.createElement('div');
+
+container.setAttribute('style', 'padding:0; width:640px; height:640px; display:flex; flex-wrap:wrap; background:black; margin-left: auto; margin-right:auto;')
+document.body.appendChild(container);
+container.classList.add('container');
+
