d3cf93a85840e40659efd34127380a7ed2946f06
Saksham <jaiswalsaksham32@gmail.com>
2022-08-11 11:01:35 +0530

    add rainbow mode, clear grid and toggle grid lines

--- javascript.js
+++ javascript.js
@@ -3,19 +3,51 @@
 const container = document.createElement('div');
 const bigContainer = document.querySelector('.bigContainer');
 const sizeButton=document.querySelector('.sizeButton');
+const rnbwButton = document.querySelector('.rnbwButton')
+const clear = document.querySelector('.clear');
+const lineBtn = document.querySelector('.lineBtn');
 
 //bigContainer.classList.add('bigContainer');
 //document.body.appendChild(bigContainer);
 sizeButton.addEventListener('click', ()=>{
     promptSize();
+});
+
+rnbwButton.addEventListener('click', () => {
+    if(rnbwButton.textContent === 'Rainbow Mode : On'){
+        rnbwButton.textContent='Rainbow Mode : Off';
+        container.classList.remove('rnbw');
+    }
+
+    else if(rnbwButton.textContent === 'Rainbow Mode : Off'){
+        rnbwButton.textContent='Rainbow Mode : On';
+        container.classList.add('rnbw');
+    }
 })
+
+lineBtn.addEventListener('click', () => {
+    if(lineBtn.textContent === 'Grid Lines : On'){
+        lineBtn.textContent='Grid Lines : Off';
+        toggleGridLinesOff();
+    }
+
+    else if(lineBtn.textContent === 'Grid Lines : Off'){
+        lineBtn.textContent='Grid Lines : On';
+        toggleGridLinesOn();
+    }
+})
+
+clear.addEventListener('click', ()=>{
+    cleanGrid();
+} )
+
 
 let sqPerSide=16;
 
 
 
 function promptSize(){
-    sqPerSide = prompt("Enter the number of squares per side (Maximum limit is 100)");
+    sqPerSide = prompt("Enter the number of squares per side (Maximum limit is 100)", "16");
     if(sqPerSide>100){
         sqPerSide=100;
     }
@@ -28,9 +60,16 @@ bigContainer.appendChild(container);
 container.classList.add('container');
 
 function changeColor(sqId){
-    console.log(sqId);
-    let anotherSq=document.getElementById(`${sqId}`);
-    anotherSq.style.backgroundColor= 'black';
+    
+    if(container.classList.contains('rnbw')){
+        let anotherSq=document.getElementById(`${sqId}`);
+        anotherSq.style.backgroundColor= randomColor();
+    }
+    
+    else{
+        let anotherSq=document.getElementById(`${sqId}`);
+        anotherSq.style.backgroundColor= 'black';
+    }
 } 
 
 function clearGrid(){
@@ -47,7 +86,7 @@ function createGrid(){
         container.appendChild(square);
         square.classList.add('sqClass');
         square.setAttribute('id', `${i}`);
-        console.log(square.id);
+        
     }
     eventListeners();
 }
@@ -87,4 +126,38 @@ function eventListeners(){
     }));
 }
 
+function randomColor() {
+    // return "#" + Math.floor(Math.random()*16777215).toString(16);
+    // this returns fewer colors but they are all nice and bright
+    return `hsl(${Math.random() * 360}, 100%, 50%)`;
+}
+
+function cleanGrid(){
+    
+    const cleanSqs=document.querySelectorAll('.sqClass');
+    cleanSqs.forEach((cleanSq => {
+        cleanSq.style.backgroundColor='white';
+    }))
+    
+}
+
+function toggleGridLinesOff(){
+    let parameters = 640/sqPerSide;
+    const cleanSqs=document.querySelectorAll('.sqClass');
+    cleanSqs.forEach((cleanSq => {
+        cleanSq.setAttribute('style', `width:${parameters}px; height:${parameters}px; background:white; border:none; box-sizing:border-box; padding:0; margin:0;`);
+    }))
+    
+}
+
+function toggleGridLinesOn(){
+    let parameters = 640/sqPerSide;
+    const cleanSqs=document.querySelectorAll('.sqClass');
+    cleanSqs.forEach((cleanSq => {
+        cleanSq.setAttribute('style', `width:${parameters}px; height:${parameters}px; background:white; border:1px solid rgba(0,0,0,0.1); box-sizing:border-box; padding:0; margin:0;`);
+    }))
+    
+}
+
 createGrid();
+
