756cb550fa77df05422861ffe48a53195b9ebd53
Saksham <jaiswalsaksham32@gmail.com>
2022-08-11 14:31:20 +0530

    add shading feature with color black only

--- javascript.js
+++ javascript.js
@@ -3,11 +3,11 @@
 const container = document.createElement('div');
 const bigContainer = document.querySelector('.bigContainer');
 const sizeButton=document.querySelector('.sizeButton');
-const rnbwButton = document.querySelector('.rnbwButton')
+const rnbwButton = document.querySelector('.rnbwButton');
 const clear = document.querySelector('.clear');
 const lineBtn = document.querySelector('.lineBtn');
 const eraser = document.querySelector('.Eraser');
-
+const shading = document.querySelector('.shading');
 //bigContainer.classList.add('bigContainer');
 //document.body.appendChild(bigContainer);
 sizeButton.addEventListener('click', ()=>{
@@ -23,6 +23,11 @@ rnbwButton.addEventListener('click', () => {
     else if(rnbwButton.textContent === 'Rainbow Mode : Off'){
         rnbwButton.textContent='Rainbow Mode : On';
         container.classList.add('rnbw');
+        if(container.classList.contains('shade'))
+        {
+            container.classList.remove('shade');
+            shading.textContent='Shading : Off';
+        }
     }
 });
 
@@ -50,6 +55,23 @@ eraser.addEventListener('click', () => {
     }
 });
 
+shading.addEventListener('click', () => {
+    if(shading.textContent === 'Shading : On'){
+        shading.textContent='Shading : Off';
+        container.classList.remove('shade');
+    }
+
+    else if(shading.textContent === 'Shading : Off'){
+        shading.textContent='Shading : On';
+        container.classList.add('shade');
+        if(container.classList.contains('rnbw'))
+        {
+            container.classList.remove('rnbw');
+            rnbwButton.textContent='Rainbow Mode : Off';
+        }
+    }
+});
+
 clear.addEventListener('click', ()=>{
     cleanGrid();
 } );
@@ -68,7 +90,7 @@ function promptSize(){
     createGrid();
 }
 
-container.setAttribute('style', 'padding:0; max-width:640px; max-height:640px; border: 20px solid rgba(60, 60, 60, 1); width:fit-content; height:fit-content; display:flex; flex-wrap:wrap; background:black; border-radius:5px;')
+container.setAttribute('style', 'padding:0; max-width:640px; max-height:640px; border: 20px solid rgba(60, 60, 60, 1); width:fit-content; height:fit-content; display:flex; flex-wrap:wrap; background:hsl(0, 0%, 0%); border-radius:5px;')
 bigContainer.appendChild(container);
 container.classList.add('container');
 
@@ -80,12 +102,16 @@ function changeColor(sqId){
     }
     else if(container.classList.contains('erase')){
         let anotherSq=document.getElementById(`${sqId}`);
-        anotherSq.style.backgroundColor= 'white';
+        anotherSq.style.backgroundColor= 'hsl(0, 0%, 100%)';
+    }
+    else if(container.classList.contains('shade')){
+        let anotherSq=document.getElementById(`${sqId}`);
+        let currentColor =rgbToHsl(anotherSq.style.backgroundColor);
+        anotherSq.style.backgroundColor= decrementLightness(currentColor);
     }
     else{
         let anotherSq=document.getElementById(`${sqId}`);
-        anotherSq.style.backgroundColor= 'black';
-        anotherSq.style.filter= 'brightness(100%)'
+        anotherSq.style.backgroundColor= 'hsl(0, 0%, 0%)';
     }
 } 
 
@@ -99,7 +125,7 @@ function createGrid(){
     let parameters = 640/sqPerSide;
     for(let i = 0; i<sqPerSide*sqPerSide; i++){
         let square=document.createElement('div')
-        square.setAttribute('style', `width:${parameters}px; height:${parameters}px; background:white; box-sizing:border-box; padding:0; margin:0;`);
+        square.setAttribute('style', `width:${parameters}px; height:${parameters}px; background:hsl(0, 0%,100%); box-sizing:border-box; padding:0; margin:0;`);
         container.appendChild(square);
         square.classList.add('sqClass');
         square.classList.add('borderClass')
@@ -179,5 +205,62 @@ function toggleGridLinesOn(){
     
 }
 
+function rgbToHsl(rgb){
+    let sep = rgb.indexOf(',') > -1 ? ',' : ' ';
+    // Turn "rgb(r,g,b)" into [r,g,b]
+    rgb = rgb.substr(4).split(')')[0].split(sep);
+    let r=rgb[0];
+    let g=rgb[1];
+    let b=rgb[2];
+
+    r /= 255;
+    g /= 255;
+    b /= 255;
+    const l = Math.max(r, g, b);
+    const s = l - Math.min(r, g, b);
+    const h = s
+        ? l === r
+        ? (g - b) / s
+        : l === g
+        ? 2 + (b - r) / s
+        : 4 + (r - g) / s
+        : 0;
+    return `(
+    ${60 * h < 0 ? 60 * h + 360 : 60 * h},
+    ${100 * (s ? (l <= 0.5 ? s / (2 * l - s) : s / (2 - (2 * l - s))) : 0)},
+    ${(100 * (2 * l - s)) / 2}
+    )`;
+}
+
+function decrementLightness(hsl){
+    let sep = hsl.indexOf(',') > -1 ? ',' : ' ';
+    // Turn "rgb(r,g,b)" into [r,g,b]
+    hsl = hsl.substr(4).split(')')[0].split(sep);
+    let h=hsl[0];
+    let s=hsl[1];
+    let l=hsl[2];
+
+    l=parseInt(l);
+    s=parseInt(s);
+    h=parseInt(h);
+
+    
+    if(l===0){
+        l=0;
+    }
+    else{
+        l-=10;
+    }
+
+    
+
+    return `hsl(${h}, ${s}%, ${l}%)`
+}
+
+
+
+
+
 createGrid();
+
 
